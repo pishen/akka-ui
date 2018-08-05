c23177f6cab460b2076788e1bf673d0320adb88e
Pishen Tsai <pishen02@gmail.com>
2018-08-06 02:30:04 +0800

    setter version

--- src/main/scala/akka/ui/package.scala
+++ src/main/scala/akka/ui/package.scala
@@ -3,22 +3,18 @@ package akka
 import akka.stream._
 import akka.stream.scaladsl._
 import org.scalajs.dom.raw._
+import scala.scalajs.js
 
 package object ui {
-  implicit class SourceBuilder[T <: EventTarget, E <: Event](t: T) {
-    def on[L <: Listener](listener: L)(
-        implicit binder: Binder[T, L, E],
-        materializer: Materializer
-    ): Source[E, akka.NotUsed] = {
-      val (queue, source) = Source
-        .queue[E](10, OverflowStrategy.dropNew)
-        .preMaterialize
+  def fromListener[E <: Event](setter: js.Function1[E, _] => Unit)(
+    implicit materializer: Materializer
+  ): Source[E, akka.NotUsed] = {
+    val (queue, source) = Source
+      .queue[E](10, OverflowStrategy.dropNew)
+      .preMaterialize
 
-      t.addEventListener[E](listener.toString, e => queue.offer(e))
-      //TODO: close the Source when T is removed
-
-      source
-    }
+    setter(e => queue.offer(e))
+    //TODO: close the Source when T is removed
+    source
   }
-
 }
