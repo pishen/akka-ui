d88cfee4b5b497e7f3e990ff6a991c039ba765b6
Pishen Tsai <pishen02@gmail.com>
2018-08-09 14:09:36 +0800

    actorRef version

--- src/main/scala/akka/ui/package.scala
+++ src/main/scala/akka/ui/package.scala
@@ -6,46 +6,41 @@ import akka.stream.scaladsl._
 import org.scalajs.dom.raw._
 import scala.scalajs.js
 import scala.reflect.ClassTag
+import scala.collection.mutable
 
 package object ui {
+  val sourceBindings = mutable.Map.empty[EventTarget, mutable.Set[ActorRef]]
+
   implicit class SourceBuilder[T <: EventTarget](t: T) {
     def source[E <: Event](selector: T => js.Function1[E, _] => Unit)(
-      implicit materializer: Materializer
+        implicit materializer: Materializer
     ): Source[E, akka.NotUsed] = {
-      val (queue, source) = Source
-        .queue[E](10, OverflowStrategy.dropNew)
+      val (eventReader, source) = Source
+        .actorRef[E](10, OverflowStrategy.dropNew)
         .preMaterialize
 
-      selector(t)(e => queue.offer(e))
+      selector(t)(e => eventReader ! e)
 
-      // Add a mark to indicate that there's a binding on this element
-      t match {
-        case (e: Element) =>
-          e.classList.add("akka-ui-binded")
-        case _ =>
-          // do nothing
-      }
-      // Complete the Source when T is removed (use custom Event)
-      t.addEventListener("akka-ui-removed", (e: Event) => queue.complete())
+      sourceBindings
+        .getOrElseUpdate(t, mutable.Set.empty[ActorRef])
+        .+=(eventReader)
 
       source
     }
   }
 
+  val sinkBindings = mutable.Map.empty[Element, mutable.Set[ActorRef]]
+
   implicit class SinkBuilder[E <: Element](e: E) {
     def sink[V: ClassTag](selector: E => V => Unit)(
-      implicit system: ActorSystem,
-      materializer: Materializer
+        implicit system: ActorSystem
     ): Sink[V, akka.NotUsed] = {
       val setter = selector(e)
       val propertyWriter = system.actorOf(PropertyWriter.props(setter))
 
-      // Kill propertyWriter when element removed
-      e.classList.add("akka-ui-binded")
-      e.addEventListener(
-        "akka-ui-removed",
-        (e: Event) => propertyWriter ! PoisonPill
-      )
+      sinkBindings
+        .getOrElseUpdate(e, mutable.Set.empty[ActorRef])
+        .+=(propertyWriter)
 
       Sink.actorRef[V](propertyWriter, PropertyWriter.Completed)
     }
