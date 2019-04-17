41e4f9c1e77105db41d4359a66c730a93ee192ab
Pishen Tsai <pishen02@gmail.com>
2019-04-17 13:29:03 +0800

    source macros first attempt

--- src/main/scala/akka/ui/package.scala
+++ src/main/scala/akka/ui/package.scala
@@ -8,23 +8,32 @@ import org.scalajs.dom.raw._
 import scala.scalajs.js
 import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.experimental.macros
+import scala.reflect.macros.whitebox.Context
 
 package object ui {
   implicit class RichDOMTokenList(tokens: DOMTokenList)
       extends EasySeq[String](tokens.length, tokens.apply)
 
-  val sourceBindings = mutable.Map
-    .empty[EventTarget, mutable.Set[SourceQueueWithComplete[_ <: Event]]]
+  val sourceBindings0 = mutable.Map.empty[EventTarget, mutable.Set[SourceQueueWithComplete[_ <: Event]]]
+  val sourceBindings =
+    mutable.Map.empty[EventTarget, mutable.Map[
+      String,
+      (SourceQueueWithComplete[_ <: Event], Source[_ <: Event, NotUsed])
+    ]]
 
   implicit class SourceBuilder[T <: EventTarget](t: T) {
-    def source[E <: Event](
+    def source(eventType: String): Any = macro SourceBuilderImpl.source[T]
+
+    def source0[E <: Event](
         selector: T => js.Function1[E, _] => Unit,
         preventDefault: Boolean = false
     )(
         implicit materializer: Materializer
     ): Source[E, NotUsed] = {
       val (eventQueue, source) = Source
-        .queue[E](10, OverflowStrategy.dropNew)
+        .queue[E](10, OverflowStrategy
+          .dropNew)
         .toMat(BroadcastHub.sink)(Keep.both)
         .run()
 
@@ -40,11 +49,63 @@ package object ui {
         case _ => //do nothing
       }
 
-      sourceBindings
+      sourceBindings0
         .getOrElseUpdate(t, mutable.Set.empty)
         .+=(eventQueue)
 
       source
+    }
+  }
+
+  object SourceBuilderImpl {
+    def source[T: c.WeakTypeTag](c: Context)(
+        eventType: c.Expr[String]
+    ): c.Expr[Any] = {
+      import c.universe._
+
+      val q"$_[$_]($elem).$_($_)" = c.macroApplication
+
+      val eventName = eventType.tree match {
+        case Literal(Constant(s: String)) => s
+        case _ =>
+          c.abort(c.enclosingPosition, "eventType must be a string literal.")
+      }
+
+      val listenerName = "on" + eventName
+      val listenerTerm = TermName(listenerName)
+
+      val E = {
+        val listener = implicitly[WeakTypeTag[T]].tpe.member(listenerTerm)
+        if (listener.fullName == "<none>") {
+          c.abort(
+            c.enclosingPosition,
+            s"Couldn't find $listenerName listener on target element."
+          )
+        } else {
+          listener.typeSignature.resultType.typeArgs.head
+        }
+      }
+
+      val res = q"""
+        import akka.stream._
+        import akka.stream.scaladsl._
+        import scala.collection.mutable
+        def createHub() = {
+          val (queue, source) = Source
+            .queue[$E](10, OverflowStrategy.dropNew)
+            .toMat(BroadcastHub.sink)(Keep.both)
+            .run()
+          $elem.$listenerTerm = e => queue.offer(e)
+          (queue, source)
+        }
+        val (queue, source) = akka.ui.sourceBindings
+          .getOrElseUpdate($elem, mutable.Map.empty)
+          .getOrElseUpdate($eventName, createHub())
+        source.asInstanceOf[Source[$E, NotUsed]]
+      """
+
+      println(showCode(res))
+      c.Expr(res)
     }
   }
 
@@ -99,12 +160,12 @@ package object ui {
                 // remove the bindings
                 (child +: child.querySelectorAll(".akka-ui-binded"))
                   .foreach { node =>
-                    sourceBindings
+                    sourceBindings0
                       .get(node)
                       .foreach { queues =>
                         queues.foreach(_.complete())
                       }
-                    sourceBindings -= node
+                    sourceBindings0 -= node
                     sinkBindings
                       .get(node)
                       .foreach { queues =>
